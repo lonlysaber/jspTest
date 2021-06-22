23e21a43040c062e5ff399641134a32ae54e6623
2019211001000909-zhangjinzhe <1312096627@qq.com>
2021-06-22 19:08:53 +0800

    week15

---
+++ src/main/java/com/zhangjinzhe/controller/AdminOrderList.java
@@ -0,0 +1,43 @@
+package com.zhangjinzhe.controller;
+
+import com.zhangjinzhe.dao.OrderDao;
+import com.zhangjinzhe.model.Order;
+import com.zhangjinzhe.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrderList", value = "/AdminOrderList")
+public class AdminOrderList extends HttpServlet {
+    private Connection con = null;
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment>paymentTypeList = Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path = "WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
